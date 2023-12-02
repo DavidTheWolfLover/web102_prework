d67b9dbab8e1fd14b99d1d1bfb2c808cdc88fdd8
Khoa Cao <kqcao@wisc.edu>
2023-12-02 11:08:40 -0600

    Challenge 4 part: Updated index.js showing game stat cards

--- index.js
+++ index.js
@@ -67,20 +67,26 @@ addGamesToPage(GAMES_JSON)
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
+let total_contri = GAMES_JSON.reduce((total, game) => {
+    return total + game["backers"]
+}, 0)
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-
+contributionsCard.innerHTML = `${total_contri.toLocaleString('en-US')}`   
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
+let total_raise = GAMES_JSON.reduce((total, game) => {
+    return total + game["pledged"]
+}, 0)
 
 // set inner HTML using template literal
-
+raisedCard.innerHTML = `$${total_raise.toLocaleString('en-US')}`   
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
-
+let total_games = GAMES_JSON.length
+gamesCard.innerHTML = `${total_games}`
 
 /*************************************************************************************
  * Challenge 5: Add functions to filter the funded and unfunded games
